7c756740f14f74d19c096e443eb9641382ce982f
Evan Henry <68968256+ehenry344@users.noreply.github.com>
2021-11-04 11:57:31 -0400

    Create init.lua

---
+++ src/Modules/init.lua
@@ -0,0 +1,65 @@
+--[[
+gilaga4815
+
+updated : 10 / 27 / 2021
+
+STUDIO SKETCH
+
+Initializer Module that handles prioritized plugin setup 
+]]
+
+local PluginUI = script.Parent.Parent.Modules.PluginUI
+local Utility = script.Parent.Parent.Modules.Utility
+
+local pluginModules = {} 
+
+-- Load Program 
+
+local function processInit(module) -- Handles The Loading Of the Modules Sequentially 
+	local moduleData = require(module) 
+	
+	if #pluginModules < 1 and not moduleData.IsMain then -- Check To Make Sure the Main Wasn't Already Inserted
+		coroutine.yield("initYield")
+	end
+	
+	pluginModules[#pluginModules + 1] = moduleData 
+		
+	return true 
+end
+
+local function startupModules(moduleFolder) -- Initialize the Singletons 
+	local coroutineTable = {} 
+	
+	for _, v in pairs(moduleFolder:GetChildren()) do 		
+		local newProcessCoroutine = coroutine.create(processInit) 
+		local success, result = coroutine.resume(newProcessCoroutine, v) 
+		
+		if type(result) == "string" then 
+			coroutineTable[#coroutineTable + 1] = newProcessCoroutine 
+		end
+	end
+	
+	for _, process in pairs(coroutineTable) do
+		coroutine.resume(process)
+	end		
+end
+
+-- Actually Start the Thing When This is invoked 
+
+return function(mainWidget, extraArgs)  -- Just Create a callable function to be created 
+	local ViewSelector = require(script.Parent.Utility.UIClasses.ViewSelector)
+	
+	startupModules(PluginUI)
+		
+	local MainFrame = pluginModules[1] 
+	MainFrame:SetParent(mainWidget) -- passes in the extra arguments regardless if they are needed or not 
+		
+	local newSelector = ViewSelector.New(MainFrame.modeMgrFrame)
+	
+	for i = 2, #pluginModules do
+		local element = pluginModules[i]	
+		element:SetParent(MainFrame.holderFrame, extraArgs)
+		
+		newSelector:CreateSelector(element.frameInstance) 
+	end
+end
